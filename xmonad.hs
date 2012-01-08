94a0acc0aa00c0ae26f386281bf34091343138f0
Andre Kelpe <efeshundertelf@googlemail.com>
2012-01-08 20:59:25 +0100

    read the xmobarrc from the git repo

--- xmonad.hs
+++ xmonad.hs
@@ -4,10 +4,11 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+import System.Environment(getEnv)
 
 
 main = do
-    xmproc <- spawnPipe "xmobar"
+    xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/xmobarrc"
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
